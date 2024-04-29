b8a521cd62e84d82c4e451b1aaf2f2ef80a08abb
Charly0400 <Carlaz020400@gmail.com>
2024-04-28 22:52:33 -0600

    Portal_animation

--- Assets/Script/Portal/Portal.cs
+++ Assets/Script/Portal/Portal.cs
@@ -6,12 +6,19 @@ public class Portal : MonoBehaviour
 {
     public GameObject portal;
     [SerializeField] private Animator animatorPortal;
+    [SerializeField] private Animator doorAnimator;
     public void Portal_ON()
     {
         portal.SetActive(true);
         animatorPortal.Play("Open");
         Debug.Log("Ok");
 
+    }
+
+    public void OpenDoor()
+    {
+        doorAnimator.Play("Open_Door");
+        Debug.Log("Open");
     }
 
 
